e759ff2d350541f997a029cf035f9498be6bfa50
ScarletFlash <scarletflash.dev@gmail.com>
2021-01-07 18:54:36 +0300

    chore: add demo project

---
+++ src/lib/button/button.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './components/button/button.component';
+
+@NgModule({
+  declarations: [ButtonComponent],
+  imports: [CommonModule],
+})
+export class ButtonModule {}
